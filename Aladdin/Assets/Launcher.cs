674a17f5c7279550ac767a2b9d20da2adfd55226
RayJi0428 <kaikai0428@gmail.com>
2019-01-08 13:06:52 +0800

    第一次上傳

---
+++ Aladdin/Assets/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject ball;
+
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+
+        if (count > 20)
+        {
+            count = 0;
+            Vector3 pos = new Vector3(transform.position.x + Random.Range(-0.1f, 0.1f), transform.position.y + Random.Range(-0.1f, 0.1f), transform.position.z);
+            Instantiate(ball, pos, transform.rotation);
+        }
+    }
+}
