592f4ee3a9a7fe7fc41c216927739a0239f89afe
RayJi0428 <kaikai0428@gmail.com>
2019-01-22 13:15:28 +0800

    新增老虎機

---
+++ Aladdin/Assets/Reel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reel : MonoBehaviour
+{
+    private GameObject container;
+
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.container = this.gameObject.transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.container.transform.Translate(0, this.speed * -1 * Time.deltaTime, 0);
+        if (this.container.transform.localPosition.y < -5.0f)
+        {
+            this.container.transform.localPosition = Vector3.zero;
+        }
+    }
+}
