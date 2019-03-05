ad3500a64c2663b6cecae7e34b2b30ae22e00fa6
RayJi0428 <kaikai0428@gmail.com>
2019-03-05 12:36:46 +0800

    [Ray] 發射球demo

---
+++ TestProject/Assets/ShootBall/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //Rigidbody目標
+    private Rigidbody rb;
+
+    //力道
+    public float power = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //取得此物件Rigidbody
+        rb = GetComponent<Rigidbody>();
+
+        //給予一次性的力道
+        rb.AddForce(Vector3.up * power, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ TestProject/Assets/ShootBall/Launcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    //生成Ball prefab物件
+    public GameObject pfBall;
+
+    private int frame = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //點擊滑鼠左鍵，生成Ball
+        if (frame == 0 || Input.GetMouseButtonUp(0))
+        {
+            frame = 10;
+            CreateBall();
+        }
+        frame--;
+    }
+
+    void CreateBall()
+    {
+        //生成Ball物件並給予隨機座標
+        Vector3 pos = new Vector3(this.transform.position.x + Random.Range(0f, 1f), this.transform.position.y, this.transform.position.z + Random.Range(0f, 1f));
+        Instantiate(pfBall, pos, Quaternion.identity);
+    }
+}
